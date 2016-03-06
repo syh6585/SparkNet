a4c654bd17b4a04341f5a240a9f177104efcc700
Robert Nishihara <robertnishihara@gmail.com>
2016-03-06 00:18:29 -0800

    remove unnecessary allocations from caffenet and add test

--- src/test/scala/libs/CaffeNetSpec.scala
+++ src/test/scala/libs/CaffeNetSpec.scala
@@ -29,7 +29,7 @@ class CaffeNetSpec extends FlatSpec {
     val schema = StructType(StructField("C0", FloatType, false) :: Nil)
     val net = CaffeNet(netParam, schema, new DefaultPreprocessor(schema))
     val inputs = List[Row](Row(0F), Row(1F))
-    val outputs = net.forward(inputs.iterator)
+    val outputs = net.forward(inputs.iterator, List("prob"))
     val keys = outputs.keys.toArray
     assert(keys.length == 1)
     assert(keys(0) == "prob")
@@ -79,5 +79,36 @@ class CaffeNetSpec extends FlatSpec {
     assert(!WeightCollection.checkEqual(net1.getWeights(), net2.getWeights(), 1e-10F)) // weights should not be equal
     net2.copyTrainedLayersFrom(sparkNetHome + "/temp/cifar10.caffemodel")
     assert(WeightCollection.checkEqual(net1.getWeights(), net2.getWeights(), 1e-10F)) // weights should be equal
+  }
+
+  "Putting input into net and taking it out" should "not change the input" in {
+    val netParam = new NetParameter()
+    ReadProtoFromTextFileOrDie(sparkNetHome + "/models/test/test.prototxt", netParam)
+    val schema = StructType(StructField("data", ArrayType(FloatType), false) :: StructField("label", IntegerType, false) :: Nil)
+    val net = CaffeNet(netParam, schema, new DefaultPreprocessor(schema))
+
+    val inputBuffer = new Array[Array[Array[Float]]](net.inputSize)
+    for (i <- 0 to net.inputSize - 1) {
+      inputBuffer(i) = new Array[Array[Float]](net.batchSize)
+      for (batchIndex <- 0 to net.batchSize - 1) {
+        inputBuffer(i)(batchIndex) = Array.range(0, net.inputBufferSize(i)).map(e => e.toFloat)
+      }
+    }
+
+    JavaCPPUtils.arraysToFloatBlobVector(inputBuffer, net.inputs, net.batchSize, net.inputBufferSize, net.inputSize) // put inputBuffer into net.inputs
+    val inputBufferOut = JavaCPPUtils.arraysFromFloatBlobVector(net.inputs, net.batchSize, net.inputBufferSize, net.inputSize) // read inputs out of net.inputs
+
+    // check if inputBuffer and inputBufferOut are the same
+    for (i <- 0 to net.inputSize - 1) {
+      var batchIndex = 0
+      while (batchIndex < net.batchSize) {
+        var j = 0
+        while (j < inputBuffer(i)(batchIndex).length) {
+          assert((inputBuffer(i)(batchIndex)(j) - inputBufferOut(i)(batchIndex)(j)).abs <= 1e-10)
+          j += 1
+        }
+        batchIndex += 1
+      }
+    }
   }
 }
